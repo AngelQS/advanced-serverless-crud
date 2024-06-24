727f208d2d883e9daff70e6c294d46b5cc7060c3
AngelQS <developer.aqs@gmail.com>
2024-06-23 19:07:49 -0500

    fix: remove unexpected token

--- src/likeUser/handler.js
+++ src/likeUser/handler.js
@@ -1,30 +1,21 @@
 const { DynamoDBClient, UpdateItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshallItem, unmarshallItem } = require('../common/dynamodb-utils');
 
-let dynamoDBClientParams = {};
+let dynamoDBClientParams = {}
 
-if (process.env.IS_OFFLINE) {
-  dynamoDBClientParams = {
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
-    accessKeyId: 'DEFAULT_ACCESS_KEY',
-    secretAccessKey: 'DEFAULT_SECRET'
-  }
-};
-
-const dynamodbClient = new DynamoDBClient(dynamoDBClientParams);
+const dynamodbClient = new DynamoDBClient(dynamoDBClientParams)
 
 const sleep = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-};
+    setTimeout(resolve, ms)
+  })
+}
 
 const likeUser = async (event, context) => {
-  console.log('event: ', event);
-  const body = event.Records[0].body;
-  const userId = JSON.parse(body).id;
-  console.log('userId: ', userId);
+  console.log('event: ', event)
+  const body = event.Records[0].body
+  const userId = JSON.parse(body).id
+  console.log('userId: ', userId)
 
   const input = {
     TableName: 'usersTable',
@@ -34,11 +25,11 @@ const likeUser = async (event, context) => {
       ':inc': 1
     }),
     ReturnValues: 'ALL_NEW'
-  };
-  const command = new UpdateItemCommand(input);
+  }
+  const command = new UpdateItemCommand(input)
 
-  const response = await dynamodbClient.send(command);
-  await sleep(4000);
+  const response = await dynamodbClient.send(command)
+  await sleep(4000)
   console.log('dynamodb response: ', response)
 };
 
